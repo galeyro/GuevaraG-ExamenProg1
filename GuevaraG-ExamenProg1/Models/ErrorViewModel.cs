88c5425b5b8db9d1965f6a0aa7c7ec244eff1828
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-04-29 07:08:55 -0500

    Add project files.

---
+++ GuevaraG-ExamenProg1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuevaraG_ExamenProg1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
