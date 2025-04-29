50a972d66e7243e6ba280528a577e7de1135165b
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-04-29 07:35:11 -0500

    Creacion de controladores y vistas a partir de modelos

--- GuevaraG-ExamenProg1/Program.cs
+++ GuevaraG-ExamenProg1/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GuevaraG_ExamenProg1.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Context") ?? throw new InvalidOperationException("Connection string 'Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
